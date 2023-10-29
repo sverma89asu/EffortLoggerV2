8060ec14e16019334152b47c26854396b971161c
Christoph Li <flerpharos@gmail.com>
2023-10-29 15:37:11 -0700

    add effort log page

---
+++ src/main/java/tu14/controllers/HomeController.java
@@ -0,0 +1,10 @@
+package tu14.controllers;
+
+import javafx.scene.control.TextField;
+
+public class HomeController {
+    public TextField effort_category;
+    public TextField deliverable;
+    public TextField lifecycle;
+    public TextField project;
+}
