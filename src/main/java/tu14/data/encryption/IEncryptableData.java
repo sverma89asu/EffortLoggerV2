eeb559e89f9cdbc2d6d74e4086961e8f596789fe
Christoph Li <flerpharos@gmail.com>
2023-11-03 21:37:10 -0700

    begin work on anonymized & encrypted data

---
+++ src/main/java/tu14/data/encryption/IEncryptableData.java
@@ -0,0 +1,12 @@
+package tu14.data.encryption;
+
+import tu14.api.IRawImplementer;
+
+/**
+ * @hidden not done yet
+ * @param <T>
+ */
+public interface IEncryptableData<T extends IRawImplementer<T>> {
+
+
+}
