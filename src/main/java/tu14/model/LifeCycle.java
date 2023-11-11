d9d24c3b4955012b3c4d8b2486c663e3676a145e
sverma89 <sverma89@asu.edu>
2023-11-10 17:33:30 -0700

    added code for effort logger

---
+++ src/main/java/tu14/model/LifeCycle.java
@@ -0,0 +1,47 @@
+package tu14.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.JsonNode;
+import tu14.api.IRawImplementer;
+
+public class LifeCycle implements IRawImplementer<LifeCycle> {
+    @JsonIgnore
+    public int id;
+    public String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private LifeCycle() {}
+    public LifeCycle(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+    @Override
+    public LifeCycle construct(JsonNode data) {
+        // TODO catch this; it could throw a runtime exception
+        this.id = data.get("id").asInt();
+        this.name = data.get("name").asText();
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
