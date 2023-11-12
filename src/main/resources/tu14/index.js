46374927e0ebb6d1d6740c2ced40fcb81079837a
Christoph Li <flerpharos@gmail.com>
2023-11-11 21:37:18 -0700

    add tables for effort and defect logs

--- src/main/resources/tu14/index.js
+++ src/main/resources/tu14/index.js
@@ -25,7 +25,7 @@ function to(_page) {
     }
 }
 
-to("effort-console");
+to("home");
 
 PetiteVue.createApp({PasswordField}).mount();
 
@@ -42,4 +42,35 @@ document.querySelector("#login-button").addEventListener("click", () => {
     } else {
         to("home");
     }
+});
+
+const effortTable = new Tabulator("#effort-table", {
+    height: "100%",
+    data: {},
+    layout: "fitColumns",
+    columns: [
+        {title: "ID", field: "id"},
+        {title: "Start Time", field: "start"},
+        {title: "End Time", field: "end"},
+        {title: "Life Cycle", field: "lifeCycle"},
+        {title: "Effort Category", field: "effortCategory"},
+        {title: "Deliverable", field: "deliverable"},
+        {title: "Project", field: "project"},
+    ]
+});
+
+const defectTable = new Tabulator("#defect-table", {
+    height: "100%",
+    data: {},
+    layout: "fitColumns",
+    columns: [
+        {title: "ID", field: "id"},
+        {title: "Name", field: "name"},
+        {title: "Status", field: "status"},
+        {title: "Fix", field: "fix"},
+        {title: "Life Cycle Included", field: "lifeCycleIncluded"},
+        {title: "Life Cycle Excluded", field: "lifeCycleExcluded"},
+        {title: "Defect Category", field: "defectCategory"},
+        {title: "Project", field: "project"},
+    ]
 });
